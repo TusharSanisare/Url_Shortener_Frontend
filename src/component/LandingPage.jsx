ee19a11ea1a99c3b5f21eb8e919b915834137526
TusharSanisare <tsanisare@gmail.com>
2025-02-07 22:43:25 +0530

    done

--- src/component/LandingPage.jsx
+++ src/component/LandingPage.jsx
@@ -75,7 +75,7 @@ const LandingPage = () => {
             viewport={{ once: true }}
             transition={{ duration: 0.8 }}
             className="sm:w-[480px] w-[400px] object-cover rounded-md"
-            src="../src/assets/images/img2.png"
+            src="/images/img2.png"
             alt=""
           />
         </div>
